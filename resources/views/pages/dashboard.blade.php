7f298fde11131ce34c196db3cce789d5b3863bcf
Jasmine Beeman <jasmine.beeman@metalab.csun.edu>
2017-08-25 17:58:14 -0700

    added a form to the page

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -31,85 +31,99 @@
   <div class="black-overlay"></div>
 </div>
 
-	<div class="tab-container">
-		<div class="container">
-		  <ul class="tabs cf">
-		    <li class="tab__list">
-		      <a class="tab__link" href="#">Home</a>
-		    </li>
-		    <li class="tab__list">
-		      <a class="tab__link" href="#">Profile</a>
-		    </li>
-		    <li class="tab__list">
-		      <a class="tab__link tab__link--active" href="#">Classes</a>
-		    </li>
-		    <li class="tab__list">
-		      <a class="tab__link" href="#">Projects</a>
-		    </li>
-		    <li class="tab__list">
-		      <a class="tab__link" href="#">Publications</a>
-		    </li>
-		    <li class="tab__list">
-		      <a class="tab__link" href="#">Committees</a>
-		    </li>
-		  </ul>
-		</div>
+<div class="tab-container">
+	<div class="container">
+	  <ul class="tabs cf">
+	    <li class="tab__list">
+	      <a class="tab__link" href="#">Home</a>
+	    </li>
+	    <li class="tab__list">
+	      <a class="tab__link" href="#">Profile</a>
+	    </li>
+	    <li class="tab__list">
+	      <a class="tab__link tab__link--active" href="#">Classes</a>
+	    </li>
+	    <li class="tab__list">
+	      <a class="tab__link" href="#">Projects</a>
+	    </li>
+	    <li class="tab__list">
+	      <a class="tab__link" href="#">Publications</a>
+	    </li>
+	    <li class="tab__list">
+	      <a class="tab__link" href="#">Committees</a>
+	    </li>
+	  </ul>
 	</div>
-	<div class="section">
-		<div class="container">
+</div>
+<div class="section">
+	<div class="container">
+		<div class="col-md-3">
+			<h5 class="">CONTACTS</5>
+			<p>888-888-888</p>
+			<p>jasmine.beeman@ gmail.com</p>
+		</div>
+
+		<div class="col-md-9">
+			<div class="table--responsive">
+			  <table class="table table--striped table--bordered table--padded table--hover">
+				  <thead>
+				    <tr>
+				      <th>Deal One</th>
+				      <th>Deal Two</th>
+				      <th>Deal Three</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <td>Col 1</td>
+				      <td>Col 2</td>
+				      <td>Col 3</td>
+				    </tr>
+				    <tr>
+				      <td>Col 1</td>
+				      <td>Col 2</td>
+				      <td>Col 3</td>
+				    </tr>
+				    <tr>
+				      <td>Col 1</td>
+				      <td>Col 2</td>
+				      <td>Col 3</td>
+				    </tr>
+				  </tbody>
+			  </table>
+			</div>
+
+		<div class="panel">
+		  <div class="panel__header">
+		    <strong>Panel</strong>
+		  </div>
+		  <div class="panel__content">This is a basic panel w/ a header
+		  <label for="ex0">Comment</label>
+		  <textarea id="ex0" name="ex0" placeholder="Comment..."></textarea>
+		  </div>
+		</div>
+
+		<h1 class="type--header type--thin type--red">Login</h1>
+		<div class="form__group">
 			<div class="row">
-				<div class="col-sm-4">
-					<h5 class="">CONTACTS</5>
-					<p>888-888-888</p>
-					<p>jasmine.beeman@ gmail.com</p>
+				<div class="col-md-6">
+					<label for="ex1">Firstname</label>
+					<input type="text" id="ex1" name="ex1" placeholder="Text...">
 				</div>
-				<div class="col-sm-8">
-					
-					<div class="table--responsive">
-					  <table class="table table--striped table--bordered table--padded table--hover">
-						  <thead>
-						    <tr>
-						      <th>Deal One</th>
-						      <th>Deal Two</th>
-						      <th>Deal Three</th>
-						    </tr>
-						  </thead>
-						  <tbody>
-						    <tr>
-						      <td>Col 1</td>
-						      <td>Col 2</td>
-						      <td>Col 3</td>
-						    </tr>
-						    <tr>
-						      <td>Col 1</td>
-						      <td>Col 2</td>
-						      <td>Col 3</td>
-						    </tr>
-						    <tr>
-						      <td>Col 1</td>
-						      <td>Col 2</td>
-						      <td>Col 3</td>
-						    </tr>
-						  </tbody>
-					  </table>
-					</div>
 
-					<div class="panel">
-					  <div class="panel__header">
-					    <strong>Panel</strong>
-					  </div>
-					  <div class="panel__content">This is a basic panel w/ a header
-					  <label for="ex0">Comment</label>
-					  <textarea id="ex0" name="ex0" placeholder="Comment..."></textarea>
-					  </div>
-					</div>
+				<div class="col-md-6">
+					<label for="ex1">Lastname</label>
+					<input type="text" id="ex1" name="ex1" placeholder="Text...">
+				</div>
+			</div>
 
+		</div>
 
-
-
-
-
-
+		<div class="form__group">
+			<div class="row">
+				<div class="col-md-12">
+					<label for="ex1">Username</label>
+					<input type="text" id="ex1" name="ex1" placeholder="Text...">
 				</div>
 			</div>
 		</div>
